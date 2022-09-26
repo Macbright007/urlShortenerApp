c83b984756ca4e93edc6ae83674bafe1f11b18b5
Macbright007 <nbright239@gmail.com>
2022-09-26 02:48:49 +0100

    updated signup and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
-import Nav from './components/Nav';
-import Main from './components/Main';
+// import Nav from './components/Nav';
+// import Main from './components/Main';
+import Design from './components/Design';
+// import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Main />
+      <Design />
+      {/* <Nav />
+      <Main /> */}
+      {/* <Login /> */}
+      <SignUp />
     </div>
   );
 }
